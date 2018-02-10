e4244544849b9afd9bb7671f0a2769de318fe21d
skygodbas <cynthia_hfe@163.com>
2018-02-10 14:01:28 +0800

    Update common.js

--- project1/libs/common.js
+++ project1/libs/common.js
@@ -1,1 +1,12 @@
-asfsagfd
+function toDouble(n){
+    return n<10?'0'+n:''+n;
+}
+
+module.exports = {
+    time2date : function(timestamp){
+        var oDate = new Date();
+        oDate.setTime(timestamp*1000);  // 设置时间（将秒转化为毫秒）
+
+        return oDate.getFullYear() + '-' + toDouble(oDate.getMonth()+1) + '-' + toDouble(oDate.getDate()) + ' ' + toDouble(oDate.getHours()) + ':' + toDouble(oDate.getMinutes()) + ':'+ toDouble(oDate.getSeconds());
+    }
+}
